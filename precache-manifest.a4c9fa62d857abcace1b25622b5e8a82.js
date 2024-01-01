0215a27062653e49cb6582cfe74a2d1efecca654
Max Greenwald <powerslab3@gmail.com>
2024-01-01 14:16:36 -0500

    Updates

---
+++ precache-manifest.a4c9fa62d857abcace1b25622b5e8a82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5199fffe88be61143669802d52e74f6c",
+    "url": "/prl4/index.html"
+  },
+  {
+    "revision": "f20aeb6cdc4bc6b53551",
+    "url": "/prl4/static/css/main.f45e64f2.chunk.css"
+  },
+  {
+    "revision": "9cf4826b2cbb34f97fa4",
+    "url": "/prl4/static/js/2.de378333.chunk.js"
+  },
+  {
+    "revision": "ba7279a4c883b4cf3de1f6b3c5a6bc14",
+    "url": "/prl4/static/js/2.de378333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20aeb6cdc4bc6b53551",
+    "url": "/prl4/static/js/main.049f577e.chunk.js"
+  },
+  {
+    "revision": "d9192727c8d69eefe7a8",
+    "url": "/prl4/static/js/runtime-main.6dc20852.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/prl3/precache-manifest.7b6d20749b65ae4647315709aaa820b4.js"
+  "/prl4/precache-manifest.a4c9fa62d857abcace1b25622b5e8a82.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prl3/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prl4/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
